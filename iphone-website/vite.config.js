d96687b50c2c35e4fe5afc9a063ff11a509862e2
Ravi Raj <ravisrivastav2609@gmail.com>
2024-08-14 00:03:46 +0530

    codss

--- iphone-website/vite.config.js
+++ iphone-website/vite.config.js
@@ -1,7 +1,24 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "self-m0a",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "self-m0a",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "self-m0a",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "self-m0a",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
